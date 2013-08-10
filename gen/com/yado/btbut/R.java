b2496e4573075ff6561bda67b4df1551f2b6b5df
Yassen Dobrev <yassendobrev@gmail.com>
2013-08-10 10:48:10 +0200

    - code cleanup - clean notifications

--- gen/com/yado/btbut/R.java
+++ gen/com/yado/btbut/R.java
@@ -42,10 +42,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int bluetooth_connected_key=0x7f050005;
-        public static final int button_remapping_key=0x7f050004;
-        public static final int hello_world=0x7f050002;
-        public static final int preferences_file_key=0x7f050003;
+        public static final int bluetooth_connected_key=0x7f050004;
+        public static final int button_remapping_key=0x7f050003;
+        public static final int preferences_file_key=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/yado/btbut/CallHandler.java
+++ src/com/yado/btbut/CallHandler.java
@@ -2,16 +2,10 @@ package com.yado.btbut;
 
 // http://android-developers.blogspot.de/2013/05/handling-phone-call-requests-right-way.html
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
-import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.view.KeyEvent;
-import android.widget.Toast;
 
 public class CallHandler extends BroadcastReceiver {
 	@Override
@@ -22,32 +16,7 @@ public class CallHandler extends BroadcastReceiver {
 		
 		if (remap)
 		{
-			// show notification that button remapping is active
-			NotificationCompat.Builder mBuilder =
-					new NotificationCompat.Builder(context)
-			.setSmallIcon(R.drawable.ic_launcher)
-			.setContentTitle("Bluetooth Button")
-			.setContentText("Button remapping active");
-
-			Intent resultIntent = new Intent(context, RemapToggleActivity.class);
-
-			TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-			// Adds the back stack for the Intent (but not the Intent itself)
-			stackBuilder.addParentStack(BtButActivity.class);
-			// Adds the Intent that starts the Activity to the top of the stack
-			stackBuilder.addNextIntent(resultIntent);
-			PendingIntent resultPendingIntent =
-					stackBuilder.getPendingIntent(
-							0,
-							PendingIntent.FLAG_UPDATE_CURRENT
-							);
-			mBuilder.setContentIntent(resultPendingIntent);
-
-			NotificationManager mNotificationManager =
-					(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-			// mId allows you to update the notification later on.
-			mNotificationManager.notify(context.getResources().getInteger(R.integer.mId), mBuilder.build());
-			
+			new MyNotification(context);
 			// http://stackoverflow.com/questions/3907062/action-media-button-does-not-work-on-real-device
 		
 			// play pause Smart AudioBook Player
